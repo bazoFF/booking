4b0a0aaa24bae272a937fc64423d8f2d050e925b
Роман Базов <r.bazov@isimplelab.com>
2022-08-09 14:59:33 +0300

    install some plugins

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import {AuthService} from "./auth/auth.service";
 import {Router} from "@angular/router";
+import {Platform} from "@ionic/angular";
+import {Capacitor} from "@capacitor/core";
+import {Geolocation} from "@capacitor/geolocation";
+import {Camera, CameraResultType} from "@capacitor/camera";
+import {Toast} from "@capacitor/toast";
 
 @Component({
   selector: 'app-root',
@@ -8,7 +13,33 @@ import {Router} from "@angular/router";
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router, private _platform: Platform) {
+    this.initApp();
+  }
+
+  initApp() {
+    this._platform.ready().then(() => {
+      // console.log('PLATFORMS:');
+      // console.log(this._platform.platforms());
+      //
+      // console.log('PLATFORM IS NATIVE?');
+      // console.log(Capacitor.isNativePlatform());
+      //
+      // Geolocation.getCurrentPosition().then(res => {
+      //   console.log('CURRENT LOCATION:');
+      //   console.log(res);
+      // });
+      //
+      // Camera.pickImages({quality: 100}).then(result => {
+      //   console.log('TAKE PHOTO:');
+      //   console.log(result);
+      // });
+      // console.log('Hello');
+      // Toast.show({
+      //   text: 'Hello!',
+      // }).then();
+    });
+  }
 
   logout() {
     this.authService.logout();
