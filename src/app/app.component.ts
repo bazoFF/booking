98f1d62dfdd74de00d9a6ffb3c35a68e8a79341f
Роман Базов <r.bazov@isimplelab.com>
2022-09-13 13:44:29 +0300

    Revert "install some plugins"
    This reverts commit 4b0a0aaa24bae272a937fc64423d8f2d050e925b.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,6 @@
 import { Component } from '@angular/core';
 import {AuthService} from "./auth/auth.service";
 import {Router} from "@angular/router";
-import {Platform} from "@ionic/angular";
-import {Capacitor} from "@capacitor/core";
-import {Geolocation} from "@capacitor/geolocation";
-import {Camera, CameraResultType} from "@capacitor/camera";
-import {Toast} from "@capacitor/toast";
 
 @Component({
   selector: 'app-root',
@@ -13,33 +8,7 @@ import {Toast} from "@capacitor/toast";
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(private authService: AuthService, private router: Router, private _platform: Platform) {
-    this.initApp();
-  }
-
-  initApp() {
-    this._platform.ready().then(() => {
-      // console.log('PLATFORMS:');
-      // console.log(this._platform.platforms());
-      //
-      // console.log('PLATFORM IS NATIVE?');
-      // console.log(Capacitor.isNativePlatform());
-      //
-      // Geolocation.getCurrentPosition().then(res => {
-      //   console.log('CURRENT LOCATION:');
-      //   console.log(res);
-      // });
-      //
-      // Camera.pickImages({quality: 100}).then(result => {
-      //   console.log('TAKE PHOTO:');
-      //   console.log(result);
-      // });
-      // console.log('Hello');
-      // Toast.show({
-      //   text: 'Hello!',
-      // }).then();
-    });
-  }
+  constructor(private authService: AuthService, private router: Router) {}
 
   logout() {
     this.authService.logout();
