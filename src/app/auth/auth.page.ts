57674e91d62d1733f12488e72ece34bb53062d9c
Роман Базов <r.bazov@isimplelab.com>
2022-09-13 18:39:52 +0300

    Add Camera plugin

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import {AuthService} from "./auth.service";
-import {Router} from "@angular/router";
-import {LoadingController} from "@ionic/angular";
-import {NgForm} from "@angular/forms";
+import { AuthService } from "./auth.service";
+import { Router } from "@angular/router";
+import { LoadingController } from "@ionic/angular";
+import { NgForm } from "@angular/forms";
 import { Contacts } from "@capacitor-community/contacts";
 import { AndroidPermissions } from "@awesome-cordova-plugins/android-permissions/ngx";
+import { CameraPreview } from "@capacitor-community/camera-preview";
 
 @Component({
   selector: 'app-auth',
@@ -14,13 +15,16 @@ import { AndroidPermissions } from "@awesome-cordova-plugins/android-permissions
 export class AuthPage implements OnInit {
   public isLoading: boolean;
   public isSignup: boolean;
+  public isRecording: boolean;
+  public imgSrc: string;
 
   constructor(
     private authService: AuthService,
     private router: Router,
     private loadingController: LoadingController,
     private _permissionsService: AndroidPermissions
-  ) { }
+  ) {
+  }
 
   get modeName() {
     return this.isSignup ? 'login' : 'signup';
@@ -49,6 +53,7 @@ export class AuthPage implements OnInit {
     console.log('switchAuthMode');
     this.isSignup = !this.isSignup;
   }
+
   async testContacts() {
     await this._permissionsService.requestPermissions([
       this._permissionsService.PERMISSION.READ_CONTACTS,
@@ -60,10 +65,43 @@ export class AuthPage implements OnInit {
     });
   }
 
+  async testCamera() {
+    await this._permissionsService.requestPermissions([
+      this._permissionsService.PERMISSION.CAMERA,
+      this._permissionsService.PERMISSION.READ_EXTERNAL_STORAGE,
+      this._permissionsService.PERMISSION.WRITE_EXTERNAL_STORAGE
+    ]);
+
+    await CameraPreview.start({
+      parent: "content",
+      position: "rear",
+      enableHighResolution: true
+    });
+
+    this.isRecording = true;
+
+    // const image = await Camera.getPhoto({
+    //   quality: 90,
+    //   allowEditing: true,
+    //   resultType: CameraResultType.Uri
+    // });
+    //
+    // this.imgSrc = image.webPath;
+    //
+    // console.log(image.base64String);
+  }
+
+  async takePhoto() {
+    const capture = await CameraPreview.capture({quality: 100});
+    this.imgSrc = capture.value;
+    await CameraPreview.stop();
+    this.isRecording = false;
+  }
+
   private _login() {
     this.isLoading = true;
     this.authService.login();
-    this.loadingController.create({keyboardClose: true, message: 'Loggin in...' })
+    this.loadingController.create({keyboardClose: true, message: 'Loggin in...'})
       .then(loadingElement => {
         loadingElement.present().then();
         setTimeout(() => {
